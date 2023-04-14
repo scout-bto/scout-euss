a1a7d2762b0d27b47b852c0d5f922cb5876aec7d
Yingli <ylou2@nrel.gov>
2023-04-14 15:20:20 -0600

    improve code

--- Scout-EUSS.py
+++ Scout-EUSS.py
@@ -2,28 +2,43 @@ from buildstock_query import BuildStockQuery
 import pandas as pd
 import numpy as np
 
-upgrade_num = 2 #define upgrade number, change this number for different upgrade packages
+upgrade_num = 3 #define upgrade number, change this number for different upgrade packages
 
 # get county name list for each EMM region
-emm_name = ['SRSE', 'SRCE', 'SRSG', 'MISS', 'SPPS', 'CANO', 'NWPP', 'CASO', 'RMRG', 
-            'ISNE', 'PJME', 'PJMD', 'FRCC', 'BASN', 'MISC', 'PJMC','MISW', 'PJMW', 
-            'SPPN', 'SPPC', 'MISE', 'NYUP', 'NYCW', 'SRCA', 'TRE']
-emm_county = []
-
 emm_mapping = pd.read_csv('county_to_emm.csv')
-for i in range (len(emm_name)):
-    emm_temp = emm_mapping.loc[emm_mapping['emm'] == emm_name[i], 'county'].values.tolist()
-    emm_county += [emm_temp]
+emm_county = {k: g["county"].tolist() for k,g in emm_mapping.groupby("emm")}
 
 my_run = BuildStockQuery(db_name='euss-final',
                         table_name='euss_res_final_2018_550k_20220901',
                         workgroup='scout',
                         buildstock_type='resstock')
 
+def end_use(building_temp, scout_col2resstock, agg_functions):
+    building = building_temp.groupby(building_temp['time']).aggregate(agg_functions)
+    #add hour of year
+    building.insert(loc=0, column='Hour of Year', value=np.arange(len(building), dtype=int)+1)
+    # delete the end use columns that doesn't have savings
+    for resstock_col_prefixes in scout_col2resstock.keys():
+        if (building[f"{resstock_col_prefixes}__savings"] >= 0).all(axis=0):
+            building = building.drop([f"{resstock_col_prefixes}__baseline"], axis=1)
+            building = building.drop([f"{resstock_col_prefixes}__savings"], axis=1)
+            
+    # save individual end use data to results
+    building_results = pd.DataFrame(columns=['Hour of Year', 'EMM Region', 'Building Type'])
+    num_end_use = int((building.shape[1]-3)/2)
+    for j in range (num_end_use):
+        building_end_use = building[['Hour of Year','EMM Region', 'Building Type']]
+        building_end_use['End Use'] = building.columns[4+j*2].split('__')[0]
+        building_end_use['Baseline'] = building[building.columns[4+j*2]]
+        building_end_use['Measure'] = building[building.columns[4+j*2]]-building[building.columns[5+j*2]]
+        frames = [building_results, building_end_use]
+        building_results = pd.concat(frames)
+    return(building_results)
+
 results = pd.DataFrame(columns=['Hour of Year', 'EMM Region', 'Building Type'])
-for i in range (len(emm_name)):
+for key in emm_county:
     #get data in 15 min timestep
-    temp_15 = my_run.savings.savings_shape(upgrade_id=upgrade_num,applied_only=True, restrict=[('county',emm_county[i])], 
+    temp_15 = my_run.savings.savings_shape(upgrade_id=upgrade_num,applied_only=True, restrict=[('county',emm_county[key])], 
                                         enduses = ['fuel_use__electricity__total__kwh',
                                                    'end_use__electricity__cooling__kwh',
                                                    'end_use__electricity__heating__kwh',
@@ -53,52 +68,37 @@ for i in range (len(emm_name)):
     temp_15.rename(columns={'geometry_building_type_recs': 'Building Type'}, inplace=True)
     
     #change the data to 60 min timestep
-    temp = pd.DataFrame(columns=['time', 'EMM Region', 'Building Type','End Use'])
-    temp['time'] = temp_15['time'].groupby(np.arange(len(temp_15))//4).first().astype(str).str[5:13]
-    temp['Building Type'] = temp_15['Building Type'].groupby(np.arange(len(temp_15))//4).first()
-    temp_other= temp_15.groupby(np.arange(len(temp_15))//4).sum()
-    temp=pd.concat([temp,temp_other],axis=1)
+    temp_60 = pd.DataFrame(columns=['time', 'EMM Region', 'Building Type','End Use'])
+    temp_60['time'] = temp_15['time'].groupby(np.arange(len(temp_15))//4).first().astype(str).str[5:13]
+    temp_60['Building Type'] = temp_15['Building Type'].groupby(np.arange(len(temp_15))//4).first()
+    temp_other_60= temp_15.groupby(np.arange(len(temp_15))//4).sum()
+    temp_60=pd.concat([temp_60,temp_other_60],axis=1)
 
-    # temp['EMM Region'] = pd.Series([emm_name[i] for x in range(len(temp.index))])
-    temp['EMM Region'] = emm_name[i] #add emm region
+    temp_60['EMM Region'] = key #add emm region
 
     #group end use data to scout-euss format
-    temp['heating__baseline'] = temp['end_use__electricity__heating__kwh__baseline'] + temp['end_use__electricity__heating_heat_pump_backup__kwh__baseline']
-    temp['heating__savings'] = temp['end_use__electricity__heating__kwh__savings'] + temp['end_use__electricity__heating_heat_pump_backup__kwh__savings']
-    temp['cooling__baseline'] = temp['end_use__electricity__cooling__kwh__baseline']
-    temp['cooling__savings'] = temp['end_use__electricity__cooling__kwh__savings']
-    temp['water heating__baseline'] = temp['end_use__electricity__hot_water__kwh__baseline']
-    temp['water heating__savings'] = temp['end_use__electricity__hot_water__kwh__savings']
-    temp['lighting__baseline'] = temp['end_use__electricity__lighting_exterior__kwh__baseline']+temp['end_use__electricity__lighting_garage__kwh__baseline']+temp['end_use__electricity__lighting_interior__kwh__baseline']
-    temp['lighting__savings'] = temp['end_use__electricity__lighting_exterior__kwh__savings']+temp['end_use__electricity__lighting_garage__kwh__savings']+temp['end_use__electricity__lighting_interior__kwh__savings']
-    temp['cooking__baseline'] = temp['end_use__electricity__range_oven__kwh__baseline']
-    temp['cooking__savings'] = temp['end_use__electricity__range_oven__kwh__savings']
-    temp['refrigeration__baseline'] = temp['end_use__electricity__freezer__kwh__baseline']+temp['end_use__electricity__refrigerator__kwh__baseline']
-    temp['refrigeration__savings'] = temp['end_use__electricity__freezer__kwh__savings']+temp['end_use__electricity__refrigerator__kwh__savings']
-    temp['clothes washing__baseline'] = temp['end_use__electricity__clothes_washer__kwh__baseline']
-    temp['clothes washing__savings'] = temp['end_use__electricity__clothes_washer__kwh__savings']
-    temp['clothes drying__baseline'] = temp['end_use__electricity__clothes_dryer__kwh__baseline']
-    temp['clothes drying__savings'] = temp['end_use__electricity__clothes_dryer__kwh__savings']
-    temp['dishwasher__baseline'] = temp['end_use__electricity__dishwasher__kwh__baseline']
-    temp['dishwasher__savings'] = temp['end_use__electricity__dishwasher__kwh__savings']
-    temp['pool heaters and pumps__baseline'] = temp['end_use__electricity__pool_heater__kwh__baseline']+temp['end_use__electricity__pool_pump__kwh__baseline']
-    temp['pool heaters and pumps__savings'] = temp['end_use__electricity__pool_heater__kwh__savings']+temp['end_use__electricity__pool_pump__kwh__savings']
-    temp['fans and pumps__baseline'] = temp['end_use__electricity__cooling_fans_pumps__kwh__baseline']+temp['end_use__electricity__heating_fans_pumps__kwh__baseline']+temp['end_use__electricity__mech_vent__kwh__baseline']
-    temp['fans and pumps__savings'] = temp['end_use__electricity__cooling_fans_pumps__kwh__savings']+temp['end_use__electricity__heating_fans_pumps__kwh__savings']+temp['end_use__electricity__mech_vent__kwh__savings']
-    temp['plug loads__baseline'] = temp['end_use__electricity__plug_loads__kwh__baseline']
-    temp['plug loads__savings'] = temp['end_use__electricity__plug_loads__kwh__savings']
-    temp['other__baseline'] = temp['end_use__electricity__ceiling_fan__kwh__baseline']+temp['end_use__electricity__hot_tub_heater__kwh__baseline']+temp['end_use__electricity__hot_tub_pump__kwh__baseline']+temp['end_use__electricity__well_pump__kwh__baseline']
-    temp['other__savings'] = temp['end_use__electricity__ceiling_fan__kwh__savings']+temp['end_use__electricity__hot_tub_heater__kwh__savings']+temp['end_use__electricity__hot_tub_pump__kwh__savings']+temp['end_use__electricity__well_pump__kwh__savings']
+    scout_col2resstock = {
+    'heating': ['end_use__electricity__heating__kwh', 'end_use__electricity__heating_heat_pump_backup__kwh'],
+    'cooling': ['end_use__electricity__cooling__kwh'],
+    'water heating': ['end_use__electricity__hot_water__kwh'],
+    'lighting': ['end_use__electricity__lighting_exterior__kwh','end_use__electricity__lighting_garage__kwh','end_use__electricity__lighting_interior__kwh'],
+    'cooking': ['end_use__electricity__range_oven__kwh'],
+    'refrigeration': ['end_use__electricity__freezer__kwh','end_use__electricity__refrigerator__kwh'],
+    'clothes washing': ['end_use__electricity__clothes_washer__kwh'],
+    'clothes drying': ['end_use__electricity__clothes_dryer__kwh'],
+    'dishwasher': ['end_use__electricity__dishwasher__kwh'],
+    'pool heaters and pumps': ['end_use__electricity__pool_heater__kwh','end_use__electricity__pool_pump__kwh'],
+    'fans and pumps': ['end_use__electricity__cooling_fans_pumps__kwh','end_use__electricity__heating_fans_pumps__kwh','end_use__electricity__mech_vent__kwh'],
+    'plug loads': ['end_use__electricity__plug_loads__kwh'],
+    'other': ['end_use__electricity__ceiling_fan__kwh','end_use__electricity__hot_tub_heater__kwh','end_use__electricity__hot_tub_pump__kwh','end_use__electricity__well_pump__kwh']  
+    }
     
     #get the data needed by Scout-EUSS
-    temp = temp[['time','EMM Region', 'Building Type','heating__baseline','heating__savings',
-                 'cooling__baseline','cooling__savings','water heating__baseline','water heating__savings',
-                 'lighting__baseline','lighting__savings','cooking__baseline','cooking__savings',
-                 'refrigeration__baseline','refrigeration__savings','clothes washing__baseline','clothes washing__savings',
-                 'clothes drying__baseline','clothes drying__savings','dishwasher__baseline','dishwasher__savings',
-                 'pool heaters and pumps__baseline','pool heaters and pumps__savings',
-                 'fans and pumps__baseline','fans and pumps__savings','plug loads__baseline','plug loads__savings',
-                 'other__baseline','other__savings']]
+    temp = temp_60[['time', 'EMM Region', 'Building Type','End Use']]
+    for col_type in ['baseline', 'savings']:
+        for resstock_col_prefixes in scout_col2resstock.keys():
+            resstock_cols = [f"{resstock_col_prefix}__{col_type}" for resstock_col_prefix in scout_col2resstock[resstock_col_prefixes]]
+            temp[f"{resstock_col_prefixes}__{col_type}"] = temp_60[resstock_cols].sum(axis=1)   
 
     agg_functions = {'time': 'first','EMM Region': 'first','Building Type': 'first',
                      'heating__baseline': 'sum', 'heating__savings': 'sum',
@@ -117,131 +117,20 @@ for i in range (len(emm_name)):
     
     #rename Mobile Home to MH
     MH_temp=temp.loc[temp['Building Type'].isin(['Mobile Home'])]
-    MH = MH_temp.groupby(MH_temp['time']).aggregate(agg_functions)
-    MH['Building Type'] = MH['Building Type'].replace('Mobile Home','MH')
-    #add hour of year
-    MH.insert(loc=0, column='Hour of Year', value=np.arange(len(MH), dtype=int)+1)
-    # delete the end use columns that doesn't have savings
-    if (MH['heating__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['heating__baseline','heating__savings'], axis=1)
-    if (MH['cooling__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['cooling__baseline','cooling__savings'], axis=1)
-    if (MH['water heating__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['water heating__baseline','water heating__savings'], axis=1)
-    if (MH['lighting__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['lighting__baseline','lighting__savings'], axis=1)
-    if (MH['cooking__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['cooking__baseline','cooking__savings'], axis=1)
-    if (MH['refrigeration__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['refrigeration__baseline','refrigeration__savings'], axis=1)
-    if (MH['clothes washing__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['clothes washing__baseline','clothes washing__savings'], axis=1)
-    if (MH['clothes drying__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['clothes drying__baseline','clothes drying__savings'], axis=1)
-    if (MH['dishwasher__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['dishwasher__baseline','dishwasher__savings'], axis=1)
-    if (MH['pool heaters and pumps__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['pool heaters and pumps__baseline','pool heaters and pumps__savings'], axis=1)
-    if (MH['fans and pumps__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['fans and pumps__baseline','fans and pumps__savings'], axis=1)
-    if (MH['plug loads__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['plug loads__baseline','plug loads__savings'], axis=1)
-    if (MH['other__savings'] >= 0).all(axis=0):
-        MH = MH.drop(['other__baseline','other__savings'], axis=1)
-    # save individual end use data to results
-    num_end_use = int((MH.shape[1]-3)/2)
-    for j in range (num_end_use):
-        MH_end_use = MH[['Hour of Year','EMM Region', 'Building Type']]
-        MH_end_use['End Use'] = MH.columns[4+j*2].split('__')[0]
-        MH_end_use['Baseline'] = MH[MH.columns[4+j*2]]
-        MH_end_use['Measure'] = MH[MH.columns[4+j*2]]-MH[MH.columns[5+j*2]]
-        frames = [results, MH_end_use]
-        results = pd.concat(frames)
+    MH_temp['Building Type'] = MH_temp['Building Type'].replace('Mobile Home','MH')
+    MH = end_use(MH_temp, scout_col2resstock, agg_functions)
     
     #aggragate Single-Family Attached and Single-Family Detached to SF
     SF_temp=temp.loc[temp['Building Type'].isin(['Single-Family Attached','Single-Family Detached'])]
-    SF = SF_temp.groupby(SF_temp['time']).aggregate(agg_functions)
-    SF['Building Type'] = SF['Building Type'].replace('Single-Family Attached','SF').replace('Single-Family Detached','SF')
-    #add hour of year
-    SF.insert(loc=0, column='Hour of Year', value=np.arange(len(SF), dtype=int)+1)
-    # delete the end use columns that doesn't have savings
-    if (SF['heating__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['heating__baseline','heating__savings'], axis=1)
-    if (SF['cooling__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['cooling__baseline','cooling__savings'], axis=1)
-    if (SF['water heating__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['water heating__baseline','water heating__savings'], axis=1)
-    if (SF['lighting__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['lighting__baseline','lighting__savings'], axis=1)
-    if (SF['cooking__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['cooking__baseline','cooking__savings'], axis=1)
-    if (SF['refrigeration__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['refrigeration__baseline','refrigeration__savings'], axis=1)
-    if (SF['clothes washing__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['clothes washing__baseline','clothes washing__savings'], axis=1)
-    if (SF['clothes drying__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['clothes drying__baseline','clothes drying__savings'], axis=1)
-    if (SF['dishwasher__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['dishwasher__baseline','dishwasher__savings'], axis=1)
-    if (SF['pool heaters and pumps__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['pool heaters and pumps__baseline','pool heaters and pumps__savings'], axis=1)
-    if (SF['fans and pumps__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['fans and pumps__baseline','fans and pumps__savings'], axis=1)
-    if (SF['plug loads__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['plug loads__baseline','plug loads__savings'], axis=1)
-    if (SF['other__savings'] >= 0).all(axis=0):
-        SF = SF.drop(['other__baseline','other__savings'], axis=1)
-    # save individual end use data to results
-    num_end_use = int((SF.shape[1]-3)/2)
-    for j in range (num_end_use):
-        SF_end_use = SF[['Hour of Year','EMM Region', 'Building Type']]
-        SF_end_use['End Use'] = SF.columns[4+j*2].split('__')[0]
-        SF_end_use['Baseline'] = SF[SF.columns[4+j*2]]
-        SF_end_use['Measure'] = SF[SF.columns[4+j*2]]-SF[SF.columns[5+j*2]]
-        frames = [results, SF_end_use]
-        results = pd.concat(frames)
-
+    SF_temp['Building Type'] = SF_temp['Building Type'].replace('Single-Family Attached','SF').replace('Single-Family Detached','SF')
+    SF = end_use(SF_temp, scout_col2resstock, agg_functions)
+    
     #aggragate Multi-Family with 2 - 4 Units and Multi-Family with 5+ Units to MF
     MF_temp=temp.loc[temp['Building Type'].isin(['Multi-Family with 2 - 4 Units','Multi-Family with 5+ Units'])]
-    MF = MF_temp.groupby(MF_temp['time']).aggregate(agg_functions)
-    MF['Building Type'] = MF['Building Type'].replace('Multi-Family with 2 - 4 Units','MF').replace('Multi-Family with 5+ Units','MF')
-    #add hour of year
-    MF.insert(loc=0, column='Hour of Year', value=np.arange(len(MF), dtype=int)+1)
-    # delete the end use columns that doesn't have savings
-    if (MF['heating__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['heating__baseline','heating__savings'], axis=1)
-    if (MF['cooling__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['cooling__baseline','cooling__savings'], axis=1)
-    if (MF['water heating__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['water heating__baseline','water heating__savings'], axis=1)
-    if (MF['lighting__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['lighting__baseline','lighting__savings'], axis=1)
-    if (MF['cooking__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['cooking__baseline','cooking__savings'], axis=1)
-    if (MF['refrigeration__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['refrigeration__baseline','refrigeration__savings'], axis=1)
-    if (MF['clothes washing__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['clothes washing__baseline','clothes washing__savings'], axis=1)
-    if (MF['clothes drying__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['clothes drying__baseline','clothes drying__savings'], axis=1)
-    if (MF['dishwasher__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['dishwasher__baseline','dishwasher__savings'], axis=1)
-    if (MF['pool heaters and pumps__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['pool heaters and pumps__baseline','pool heaters and pumps__savings'], axis=1)
-    if (MF['fans and pumps__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['fans and pumps__baseline','fans and pumps__savings'], axis=1)
-    if (MF['plug loads__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['plug loads__baseline','plug loads__savings'], axis=1)
-    if (MF['other__savings'] >= 0).all(axis=0):
-        MF = MF.drop(['other__baseline','other__savings'], axis=1)
-    # save individual end use data to results
-    num_end_use = int((MF.shape[1]-3)/2)
-    for j in range (num_end_use):
-        MF_end_use = MF[['Hour of Year','EMM Region', 'Building Type']]
-        MF_end_use['End Use'] = MF.columns[4+j*2].split('__')[0]
-        MF_end_use['Baseline'] = MF[MF.columns[4+j*2]]
-        MF_end_use['Measure'] = MF[MF.columns[4+j*2]]-MF[MF.columns[5+j*2]]
-        frames = [results, MF_end_use]
-        results = pd.concat(frames)
+    MF_temp['Building Type'] = MF_temp['Building Type'].replace('Multi-Family with 2 - 4 Units','MF').replace('Multi-Family with 5+ Units','MF')
+    MF = end_use(MF_temp, scout_col2resstock, agg_functions)
+    
+    frames = [results,MH, SF, MF]
+    results = pd.concat(frames)
 
 results.to_csv('upgrade'+str(upgrade_num)+'.csv', index=False)
